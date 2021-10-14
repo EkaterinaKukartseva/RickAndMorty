2b7ec6efe30f6a166ed7f51bdb1a90c70ed8d03a
Екатерина <sos91195@mail.ru>
2021-10-14 23:05:20 +0300

    added Sourcery to generate mock data

---
+++ RickAndMortyTests/Mock/Extensions/TestsExtentions.swift
@@ -0,0 +1,22 @@
+//
+//  TestsExtentions.swift
+//  RickAndMortyTests
+//
+//  Created by EKATERINA  KUKARTSEVA on 14.10.2021.
+//
+
+import Foundation
+@testable import RickAndMorty
+
+// sourcery:file: AutoMockable
+
+// MARK: - Home
+// MARK: - EpisodeList
+// MARK: - EpisodeDetails
+// MARK: - EpisodeListPagination
+// MARK: - LocationDetails
+// MARK: - LocationListPagination
+// MARK: - CharacterList
+// MARK: - CharacterDetails
+// MARK: - CharacterListPagination
+
---
+++ RickAndMortyTests/Mock/Generated/AutoMockable.generated.swift
@@ -0,0 +1,9 @@
+// Generated using Sourcery 1.6.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
